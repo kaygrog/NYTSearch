ced3b9b90122d416c5537dfaa39cbe4720a2e26b
ggank <13955056+kaygrog@users.noreply.github.com>
2020-03-30 21:30:26 -0500

    Add search.js file

--- search.js
+++ search.js
@@ -4,13 +4,31 @@
 // 3. Create ajax call and pass in query URL and "GET" method
 // 4. Create .then function and pass in ajax response
 // 5. Console log the abstract from the ajax response
+var topArticles = $('#top-articles')
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=KeGskohuzkW74NHKCcYAKAOqyGG89WXw&limit=3";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      console.log(response);
+
+$('#search').on('click', function() {
+    var searchTerm = $('#search-term').val()
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=KeGskohuzkW74NHKCcYAKAOqyGG89WXw&limit=3";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
     })
+        .then(function(hotsauce) {
+            // Grab headline and authors from response
+            // Insert into "top-articles" div
+            var responseArray = hotsauce.response.docs;
+            for (var i=0; i < 4; i++) {
+                topArticles.html(responseArray[i]);
+            }
+
+            console.log(responseArray[0].abstract);
+            
+
+            
+        })
+})
+
+    
